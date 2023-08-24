dd57f3e86b9d06f8c70ea90184d4c573e4506d5d
S4TURN <s4turn21@gmail.com>
2023-08-24 17:27:24 +0300

    feat: add Circle shape

---
+++ Mindbox.CSharpTask/Mindbox.CSharpTask/Shapes/Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mindbox.CSharpTask
+{
+    /// <summary>
+    /// Circle shape
+    /// </summary>
+    public class Circle : IShape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        /// <summary>
+        /// Calculates area of a circle
+        /// </summary>
+        /// <returns>Area of a circle</returns>
+        public double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
