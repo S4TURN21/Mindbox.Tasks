a5a8d6c2395b41df72d022c481f8e6c24340afdc
S4TURN <s4turn21@gmail.com>
2023-08-24 16:25:53 +0300

    feat: add IShape interface

---
+++ Mindbox.CSharpTask/Mindbox.CSharpTask/Interfaces/IShape.cs
@@ -0,0 +1,14 @@
+﻿namespace Mindbox.CSharpTask
+{
+    /// <summary>
+    /// Interface for shape. Provides shape's area.
+    /// </summary>
+    public interface IShape
+    {
+        /// <summary>
+        /// Calculates area of the shape
+        /// </summary>
+        /// <returns>Area of the shape</returns>
+        public double GetArea();
+    }
+}
